c0d03b4d004889ee6d341c21635a6e3882e804f5
Ricardo Luiz Silveira <ricardo.silveira@matera.com>
2020-06-22 20:52:40 -0300

    Selector, subscriber and producer

---
+++ src/br/com/projetochernobyl/jms/selector/TestConsumerTopicStoreSelector.java
@@ -0,0 +1,55 @@
+package br.com.projetochernobyl.jms.selector;
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+
+public class TestConsumerTopicStoreSelector {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws JMSException, NamingException {
+		InitialContext context = new InitialContext();
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		Connection conn = factory.createConnection();
+		conn.setClientID("Store");
+		conn.start();
+		
+		Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Topic topic = (Topic) context.lookup("loja");
+		
+		MessageConsumer consumer = session.createDurableSubscriber(topic, "sibcription-selector", "ebook=false", false);
+		consumer.setMessageListener(new MessageListener() {
+
+			@Override
+			public void onMessage(Message message) {
+				TextMessage text = (TextMessage) message;
+				try {
+					System.out.println("Message: " + text.getText());
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		});
+
+		new Scanner(System.in).nextLine();
+		
+		session.close();
+		conn.close();
+		context.close();
+		System.out.println("Terminated!");
+
+	}
+
+}
---
+++ src/br/com/projetochernobyl/jms/selector/TestProducerTopicSelector.java
@@ -0,0 +1,44 @@
+package br.com.projetochernobyl.jms.selector;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+
+public class TestProducerTopicSelector {
+
+	public static void main(String[] args) throws JMSException, NamingException {
+		InitialContext context = new InitialContext();
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		Connection conn = factory.createConnection();
+		conn.start();
+		
+		
+		// get consumer from session, it requires a Destination (Queue)
+		Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Destination topic = (Destination) context.lookup("loja");
+		
+		// from producer we are able to send a message
+		MessageProducer producer = session.createProducer(topic);
+		
+		Message message1 = session.createTextMessage("<id>111</id><name>joão</name>");
+		producer.send(message1);
+		
+		Message message2 = session.createTextMessage("<id>111</id><e-book>false</e-book><name>joão</name>");
+		message2.setBooleanProperty("ebook", false);
+		producer.send(message2);
+		
+		session.close();
+		conn.close();
+		context.close();
+		System.out.println("Terminated!");
+
+	}
+
+}
