ddf559c4797d405f3b0e8bc5b42c98e288b56dd9
Ricardo Luiz Silveira <ricardo.silveira@matera.com>
2020-06-22 22:33:13 -0300

    DLQ Queue

---
+++ src/br/com/projetochernobyl/jms/queue/dlq/TestConsumerDLQ.java
@@ -0,0 +1,54 @@
+package br.com.projetochernobyl.jms.queue.dlq;
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class TestConsumerDLQ {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws JMSException, NamingException {
+
+		InitialContext context = new InitialContext();
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		Connection conn = factory.createConnection();
+		conn.start();
+
+		// get consumer from session, it requires a Destination (Queue)
+		Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Destination queue = (Destination) context.lookup("DLQ");
+
+		// from consumer we are able to get a message
+		MessageConsumer consumer = session.createConsumer(queue);
+		consumer.setMessageListener(new MessageListener() {
+
+			@Override
+			public void onMessage(Message message) {
+				TextMessage text = (TextMessage) message;
+				try {
+					System.out.println("Message: " + text.getText());
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+			}
+
+		});
+
+		new Scanner(System.in).nextLine();
+
+		session.close();
+		conn.close();
+		context.close();
+		System.out.println("Terminated!");
+
+	}
+}
